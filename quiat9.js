8a374a2e1a4fa75eafee7186a6a515c6f32e673d
thedudec <103147375+thedudec@users.noreply.github.com>
2022-04-06 13:50:44 -0700

    Update quiat9.js

--- quiat9.js
+++ quiat9.js
@@ -1,37 +1,45 @@
-Qualtrics.SurveyEngine.addOnload(function () {
-    // hide question and next button
-    var container = this.getQuestionContainer();
-    container.querySelector('.Inner').style.display = 'none';
-    this.hideNextButton();
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
 
-    // load MinnoJS from the CDN (you probably don't need to change this)
-    var scriptTag = document.createElement('script');
-    scriptTag.src = 'https://cdn.jsdelivr.net/gh/minnojs/minno-quest@0.3/dist/pi-minno.js';
-    scriptTag.onload = onLoad;
-    scriptTag.onreadystatechange = onLoad;
-    container.appendChild(scriptTag);
+	return iatExtension({
+		category1 : {
+			name : 'Black people', //Will appear in the data.
+			title : {
+				media : {word : 'Black people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'White people', //Will appear in the data.
+			title : {
+				media : {word : 'White people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'yf1.jpg'}, 
+    			{image : 'yf4.jpg'}, 
+    			{image : 'yf5.jpg'}, 
+    			{image : 'ym2.jpg'}, 
+    			{image : 'ym3.jpg'}, 
+    			{image : 'ym5.jpg'}			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
 
-    // create the root element for Minno
-    var canvas = document.createElement('div');
-    container.appendChild(canvas);
-
-    // function to proceed to next question
-    var proceed = this.clickNextButton.bind(this);
-
-    // This function gets activated only after MinnoJS is loaded
-    function onLoad() {
-        // Run your study (just set the correct URL)
-        minnoJS(canvas, 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/exampleQIAT.js');
-
-        // MinnoJS doesn't know about Qualtrics, we pass a function to inject the results into the question
-        // For some reason `piGlobal` isn't available so we attach it to `minnoJS`
-        minnoJS.logger = function (value) {
-            var el = container.querySelector('textarea');
-            el.value = value;
-        }
-
-        // At the end of the study let MinnoJS proceed to the next question
-        // We need to wait a few miliseconds for Qualtrics to register the value that we entered
-        minnoJS.onEnd = function () { setTimeout(proceed, 100); }
-    }
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
 });
